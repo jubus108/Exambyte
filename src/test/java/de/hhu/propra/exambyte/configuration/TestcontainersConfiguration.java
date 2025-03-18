0538a0324de694ea36a5a3634d72ffbbd9aea8d7
jubus108 <jubus108@uni-duesseldorf.de>
2025-03-18 15:49:08 +0100

    Dependencies gefixed und DB zum laufen gebracht

---
+++ src/test/java/de/hhu/propra/exambyte/configuration/TestcontainersConfiguration.java
@@ -0,0 +1,15 @@
+package de.hhu.propra.exambyte.configuration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration
+public class TestcontainersConfiguration {
+
+//    @Bean
+//    @ServiceConnection
+//    PostgreSQLContainer<?> postgresContainer() {
+//        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
+//    }
+}
